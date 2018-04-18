d56373af80bc61029accfa756808d4f9f0edce05
dawoodchouhan <dawoodchauhan.93@gmail.com>
2018-04-18 23:39:38 +0530

    Basic Setup

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'MD Dawood Ibrahim',
+    username: 'Dawood',
+    email: 'dawood@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Shashank Kumar',
+    username: 'Shashank',
+    email: 'Shashank@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Vinay Kumar',
+    username: 'Vinay',
+    email: 'Vinay@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Akshay Jala',
+    username: 'Akshay',
+    email: 'Akshay@yahoo.com'
+  },
+  {
+    id: 5,
+    name: 'Chandra Shekhar',
+    username: 'Chandu',
+    email: 'Chandu@hotmail.com'
+  },
+  {
+    id: 6,
+    name: 'Ganesh Yadav',
+    username: 'Ganesh',
+    email: 'ganesh@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Prakash laddu',
+    username: 'Prakash',
+    email: 'Prakash@yahpo.com'
+  },
+  {
+    id: 8,
+    name: 'Praveen Kumar',
+    username: 'Praveen',
+    email: 'praveen@yahoo.com'
+  },
+  {
+    id: 9,
+    name: 'Melinda Vinod',
+    username: 'Melinda',
+    email: 'Melinda@haha.com'
+  },
+  {
+    id: 10,
+    name: 'Rakesh Kumar',
+    username: 'Rakesh',
+    email: 'Rakesh@anger.com'
+  }
+];
